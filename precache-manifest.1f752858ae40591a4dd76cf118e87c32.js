a8d0e80504da0814fa67e1d309db1e5dd823e7d2
gutsyheart-10 <gutsyheart10@gmail.com>
2020-06-09 18:19:03 +0600

    Updates

---
+++ precache-manifest.1f752858ae40591a4dd76cf118e87c32.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c6338c930222ea09c3baf5c5f8ff37",
+    "url": "/Resume-/index.html"
+  },
+  {
+    "revision": "e275df115e1ab621be97",
+    "url": "/Resume-/static/css/main.081b5f90.chunk.css"
+  },
+  {
+    "revision": "a22106e7579f46d6bccd",
+    "url": "/Resume-/static/js/2.b17278d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Resume-/static/js/2.b17278d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e275df115e1ab621be97",
+    "url": "/Resume-/static/js/main.64491464.chunk.js"
+  },
+  {
+    "revision": "82caa11eb5ae78fa3162",
+    "url": "/Resume-/static/js/runtime-main.1c8b364d.js"
+  },
+  {
+    "revision": "4ab5686797d945b5b3a2a443ced96c10",
+    "url": "/Resume-/static/media/HTML5.4ab56867.jpg"
+  },
+  {
+    "revision": "27421b31b0759399d236ca61f6a9a6cd",
+    "url": "/Resume-/static/media/apple.27421b31.png"
+  },
+  {
+    "revision": "a8aac10454b3339639ff11a79e436083",
+    "url": "/Resume-/static/media/backgorund.a8aac104.jpg"
+  },
+  {
+    "revision": "815921668fe00d22aeebbfc24eceb27f",
+    "url": "/Resume-/static/media/css.81592166.jpg"
+  },
+  {
+    "revision": "c1ea2b229ff3063c6dcb40fbfa2ba982",
+    "url": "/Resume-/static/media/english.c1ea2b22.jpg"
+  },
+  {
+    "revision": "8cd19d64f723008347841111c3f8303c",
+    "url": "/Resume-/static/media/figma.8cd19d64.jpeg"
+  },
+  {
+    "revision": "265d950c75731410dd5e0811a6bce252",
+    "url": "/Resume-/static/media/git.265d950c.png"
+  },
+  {
+    "revision": "576faa1a05d9648022c663c7b96c4486",
+    "url": "/Resume-/static/media/header.576faa1a.jpg"
+  },
+  {
+    "revision": "0e4fdce8ac22e09688c580e5bc4dcd7d",
+    "url": "/Resume-/static/media/js.0e4fdce8.png"
+  },
+  {
+    "revision": "a0156cae1672ac011b3cd3ce6e39dbff",
+    "url": "/Resume-/static/media/json.a0156cae.webp"
+  },
+  {
+    "revision": "30ee0ec9836601efe21c1f8f2471271c",
+    "url": "/Resume-/static/media/kyrgyzstan.30ee0ec9.png"
+  },
+  {
+    "revision": "80e73cc7b4af0198bf1a2ffe04004ffb",
+    "url": "/Resume-/static/media/mariposa.80e73cc7.png"
+  },
+  {
+    "revision": "9fb5782aff482a97ff5107f1b0cb2610",
+    "url": "/Resume-/static/media/react.9fb5782a.jpeg"
+  },
+  {
+    "revision": "e40ab1d29e75a198e107844a36ff230b",
+    "url": "/Resume-/static/media/talgat.e40ab1d2.png"
+  },
+  {
+    "revision": "225fbbe09689a4a3219ae1e00fe2280b",
+    "url": "/Resume-/static/media/vscode.225fbbe0.png"
+  }
+]);
