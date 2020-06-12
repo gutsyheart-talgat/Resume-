aec2a886e14615c09ed552d6ebfd8953fd2bf0b7
gutsyheart-10 <gutsyheart10@gmail.com>
2020-06-12 18:18:07 +0600

    done

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,25 +14,31 @@ export default function About (){
             <Nav />
             <div className="back">
                 <div className="container about" >
-                        <p className="about-text col-12 col-md-6 col-lg-6">
-                            <span className="text-me">Salam) My name is Talgat. I was born and raised in Bishkek.</span>
-                            
-                            <span className="text-me">I study at ECSG # 70, in a class with a mathematical bias.</span>
-                            
-                            <span className="text-me">As a child, I loved to count and add, so I was sent to this class. </span>
-                            
-                            <span className="text-me">I studied well and am studying It is quite easy for me.</span>
-                            
-                            <span className="text-me">I also love sports. He was engaged in many sports. For instance:</span>
-                            
-                            <span className="text-me">Football - although I never learned to play well (</span>
-                        
-                            <span className="text-me">Swimming - I participated in many sports events</span>
-                            
-                            <span className="text-me">Judo - didn’t do it for long because of an injury,</span>
-                            <span className="text-me">but managed to get prize in competitions </span>
-                        </p>
-                        <img className="foto col-12 col-md-6 col-lg-6" src={foto}/>
+                        <div className="about-cont">
+                            <img className="foto" src={foto}/> 
+                        </div>
+                        <div className="about-cont">
+                            <div className="about-text">
+                                <p className="text-me">Salam) My name is Talgat. I was born and raised in Bishkek.</p>
+                                    
+                                <p className="text-me">I study at ECSG # 70, in a class with a mathematical bias.</p>
+                                    
+                                <p className="text-me">As a child, I liked to count and add, and I did it better than others.</p>
+                                
+                                <p className="text-me">so I was sent to this class. </p>
+
+                                <p className="text-me">I studied well and am studying It is quite easy for me.</p>
+                                    
+                                <p className="text-me">I also love sports. He was engaged in many sports. For instance:</p>
+                                    
+                                <p className="text-me">Football - although I never learned to play well (</p>
+                                
+                                <p className="text-me">Swimming - I participated in many sports events</p>
+                                    
+                                <p className="text-me">Judo - didn’t do it for long because of an injury,</p>
+                                <p className="text-me">but managed to get prize in competitions </p>
+                            </div>
+                        </div>
                 </div>
             </div>
         </div>
